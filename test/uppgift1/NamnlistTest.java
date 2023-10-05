8a4b7ecb1ee30d2cc11c727407f717cd18bcf610
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2023-10-05 19:01:37 +0200

    Main

---
+++ test/uppgift1/NamnlistTest.java
@@ -0,0 +1,34 @@
+package uppgift1;
+
+import org.junit.jupiter.api.Test;
+
+class NamnlistTest {
+
+    Växter v = new Kaktus();
+
+    String name;
+    double längd;
+    NamnlistTest(String namn, double längd){
+        this.name = namn;
+        this.längd = längd;
+    }
+    @Test
+    void getNamn() {
+    }
+
+    @Test
+    void getLängd() {
+    }
+
+    @Test
+    void getKaktus() {
+    }
+
+    @Test
+    void getPalm() {
+    }
+
+    @Test
+    void getKöttVäxt() {
+    }
+}
